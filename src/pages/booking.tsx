5620e125b25314c61138f91fae5dc35104f46f8b
Craftgirlsss <craftgirlsssshopping@gmail.com>
2025-06-13 17:08:32 +0700

    okay

---
+++ src/pages/booking.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import BookingPage from './BookingPage'
+import '../index.css'
+
+createRoot(document.getElementById("root")!).render(<BookingPage />); 
---
+++ src/pages/main.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client'
+import Index from './Index'
+import '../index.css'
+
+createRoot(document.getElementById("root")!).render(<Index />); 
